5dc28b696d21df7621cc12c030caa288381caa2e
lys1k <lysikadrianna@gmail.com>
2022-05-08 16:30:58 +0200

    create genetic algorithm

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -7,24 +7,31 @@ class GeneticAlgorithm:
     def __init__(self, population):
         self.population = population
 
-    EPOCHS_NUM = 500
+    EPOCHS_NUM = 50
     MUTATION_INCREASE = 0.001
 
     CHILDREN_NUM = 20
     CHILDREN_INCREASE = 2
 
-    MUTATION_RATE = 0.1
+    MUTATION_RATE = 0.3
     MUTATION_CHANGE_OVER_EPOCHS = 120
 
     NO_IMPROVES = 80
 
+    def fitness(self, team):
+        return self.population.fitness(team)
+
+    def get_random_genes(self):
+        return self.population.get_random_team_genes()
+
     def crossover(self, team1, team2):
-        genes = self.population.get_random_team_genes()
+        genes = self.get_random_genes()
         new_team = Team(genes)
-        idx = randint(0, 10)
+        team_size = team1.size
+        idx = randint(0, team_size-1)
         for i in range(idx):
             new_team.set_gene(i, team1.get_gene(i))
-        for i in range(idx, 11):
+        for i in range(idx, team_size):
             new_team.set_gene(i, team2.get_gene(i))
         return new_team
 
@@ -40,12 +47,11 @@ class GeneticAlgorithm:
 
         return Team(genes)
 
-    def generate_best_team_mutation(self):
-        genes = self.population.get_random_team_genes()
-        team = Team(genes)
+#TODO: Wybierać najelpsze geny od rodziców
+    def generate_best_team_mutation(self, team):
         stable_score = 0
         best_team = team
-        best_score = self.population.fitness(team)
+        best_score = self.fitness(team)
         for i in range(self.EPOCHS_NUM):
             improve = False
             if stable_score == self.NO_IMPROVES:
@@ -54,7 +60,7 @@ class GeneticAlgorithm:
                 self.CHILDREN_NUM += self.CHILDREN_INCREASE
             for j in range(self.CHILDREN_NUM):
                 child = self.mutate(len(team.genes))
-                child_score = self.population.fitness(child)
+                child_score = self.fitness(child)
                 if child_score >= best_score:
                     best_score = child_score
                     best_team = child
@@ -63,14 +69,9 @@ class GeneticAlgorithm:
                 stable_score += 1
         return best_team
 
-    def generate_best_team_crossover(self):
-        genes = self.population.get_random_team_genes()
-        team1 = Team(genes)
-        genes = self.population.get_random_team_genes()
-        team2 = Team(genes)
+    def generate_best_team_crossover(self, team1, team2):
         no_improves = 0
-
-        if self.population.fitness(team1) > self.population.fitness(team2):
+        if self.fitness(team1) > self.fitness(team2):
             better_team = team1
             worse_team = team2
         else:
@@ -83,13 +84,28 @@ class GeneticAlgorithm:
                 self.CHILDREN_NUM += self.CHILDREN_INCREASE
             for j in range(self.CHILDREN_NUM):
                 child = self.crossover(better_team, worse_team)
-                if self.population.fitness(child) > self.population.fitness(better_team):
+                if self.fitness(child) > self.fitness(better_team):
                     worse_team = better_team
                     better_team = child
                     improve = True
-                elif self.population.fitness(child) > self.population.fitness(worse_team):
+                elif self.fitness(child) > self.fitness(worse_team):
                     worse_team = child
             if not improve:
                 no_improves += 1
 
-        return better_team
+        return better_team, worse_team
+
+    def generate_best_team(self):
+        genes = self.get_random_genes()
+        team1 = Team(genes)
+        genes = self.get_random_genes()
+        team2 = Team(genes)
+        for i in range(self.EPOCHS_NUM):
+            child1, child2 = self.generate_best_team_crossover(team1, team2)
+            team1 = self.generate_best_team_mutation(child1)
+            team2 = self.generate_best_team_mutation(child2)
+
+        return team1 if self.fitness(team1) > self.fitness(team2) else team2
+
+
+
--- population.py
+++ population.py
@@ -6,19 +6,21 @@ class Population:
         self.players = players
         self.teams = teams
 
+# TODO: dla use_best uwzględniać aux
     def generate_player(self, position, use_best=False):
         players = self.players
         players_list = sorted(players, key=lambda x: x["ratings"][position], reverse=True)
         if use_best:
             player = players.index(players_list[0])
         else:
-            player = randint(0,len(players_list)-1)
+            player = randint(0, len(players_list) - 1)
         return player
 
     def get_random_team_genes(self):
-        idx = randint(0,len(self.teams)-1)
+        idx = randint(0, len(self.teams)-1)
         return self.teams[idx]
 
+# TODO: uzględniać aux, liczyć koszta itp
     def fitness(self, team):
         fitness_score = 0
         idx = 0
